4ac6c6ee94e352c4f7a2cbb3a1eb68d6f5bc710c
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-02-24 21:30:43 +0100

    header styling and image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
+import Icon from "../assets/icon.jpeg";
+
 const Header = () => {
   return (
-    <div className="bg-[#795B82] py-6 flex flex-row justify-evenly sticky">
-      <b className="cursor-pointer">About me</b>
-      <b>Technologies</b>
-      <b>Projects</b>
-      <b>Experience</b>
-      <b>Contact</b>
+    <div className="bg-[#795B82] flex flex-row sticky">
+      <div className="w-[10%] ml-12 flex items-center">
+        <img src={Icon} alt="Lily of the valley" className="h-12 w-auto" />
+      </div>
+      <div className="w-[90%] flex justify-evenly py-6">
+        <a href="/AboutMe" className="cursor-pointer">
+          About me
+        </a>
+        <a href="/Technologies" className="cursor-pointer">
+          Technologies
+        </a>
+        <a href="/Projects" className="cursor-pointer">
+          Projects
+        </a>
+        <a href="/Experience" className="cursor-pointer">
+          Experience
+        </a>
+        <a href="/Contact" className="cursor-pointer">
+          Contact
+        </a>
+      </div>
     </div>
   );
 };
