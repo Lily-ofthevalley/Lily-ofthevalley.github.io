a963aa0ba68a1dfdceadef937baf624ab2c9fb1c
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-02-24 21:12:47 +0100

    started header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="bg-[#795B82] py-6 flex flex-row justify-evenly sticky">
+      <b className="cursor-pointer">About me</b>
+      <b>Technologies</b>
+      <b>Projects</b>
+      <b>Experience</b>
+      <b>Contact</b>
+    </div>
+  );
+};
+
+export default Header;
