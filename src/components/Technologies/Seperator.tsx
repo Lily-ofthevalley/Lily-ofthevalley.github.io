a0405448606c6f1508cf901459c4ee39ea70cd0a
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-02-25 20:06:36 +0100

    Update Seperator.tsx

--- src/components/Technologies/Seperator.tsx
+++ src/components/Technologies/Seperator.tsx
@@ -5,7 +5,7 @@ interface Props {
 const Seperator = ({ title }: Props) => {
   return (
     <div className="border-b-2 border-white w-9/10 flex justify-start mb-4">
-      <b className="text-2xl p-4">{title}</b>
+      <b className="text-3xl p-4">{title}</b>
     </div>
   );
 };
