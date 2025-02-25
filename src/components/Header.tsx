0e650335a4767ac09d60116b2521e0d8f4ba814d
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-02-25 19:22:00 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,16 @@ import Icon from "../assets/icon.jpeg";
 
 const Header = () => {
   return (
-    <div className="bg-[#795B82] flex flex-row sticky">
+    <div className="bg-[#795B82] flex flex-row sticky w-full">
       <div className="w-[10%] ml-12 flex items-center">
         <img src={Icon} alt="Lily of the valley" className="h-12 w-auto" />
       </div>
       <div className="w-[90%] flex justify-evenly py-6">
-        <a href="/AboutMe" className="cursor-pointer">
-          About me
-        </a>
-        <a href="/Technologies" className="cursor-pointer">
-          Technologies
-        </a>
-        <a href="/Projects" className="cursor-pointer">
-          Projects
-        </a>
-        <a href="/Experience" className="cursor-pointer">
-          Experience
-        </a>
-        <a href="/Contact" className="cursor-pointer">
-          Contact
-        </a>
+        <a href="/AboutMe">About me</a>
+        <a href="/Technologies">Technologies</a>
+        <a href="/Projects">Projects</a>
+        <a href="/Experience">Experience</a>
+        <a href="/Contact">Contact</a>
       </div>
     </div>
   );
