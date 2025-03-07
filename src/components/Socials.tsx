c51cbf8c122d869aff004821894c4ae6735d5d66
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-07 22:49:00 +0100

    Update Socials.tsx

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -7,16 +7,28 @@ const Socials = () => {
     <aside className="left-0 z-30 h-screen fixed flex items-center">
       <div className="bg-[#DDB6C6] h-1/3 rounded-r-xl flex flex-col justify-evenly shadow-lg">
         <a href="https://github.com/Lily-ofthevalley" target="_blank">
-          <img src={github} alt="Github logo" className="w-auto h-9 m-3" />
+          <img
+            src={github}
+            alt="Github logo"
+            className="w-auto h-9 m-3 duration-300 ease-in-out hover:scale-115"
+          />
         </a>
         <a
           href="https://www.linkedin.com/in/lilith-van-der-plas-947809305/"
           target="_blank"
         >
-          <img src={linkedin} alt="Linkedin logo" className="w-auto h-9 m-3" />
+          <img
+            src={linkedin}
+            alt="Linkedin logo"
+            className="w-auto h-9 m-3 duration-300 ease-in-out hover:scale-115"
+          />
         </a>
         <a href="mailto:lilithvdplas@gmail.com" target="_blank">
-          <img src={mail} alt="Mail logo" className="w-auto h-9 m-3" />
+          <img
+            src={mail}
+            alt="Mail logo"
+            className="w-auto h-9 m-3 duration-300 ease-in-out hover:scale-115"
+          />
         </a>
       </div>
     </aside>
