c146fc9a4d34c98e62517be6f4d3ee251249e983
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-07 22:59:27 +0100

    Update Experience.tsx

--- src/layouts/Experience.tsx
+++ src/layouts/Experience.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Experience = () => {
-  return <div></div>;
+  return (
+    <div className="w-full mt-6 rounded-xl flex flex-col items-center">
+      <h1 className="text-5xl text-[#AC8DAF]">Experience/Education</h1>
+      <div className="w-10/12 mt-9 grid grid-cols-4"></div>
+    </div>
+  );
 };
 
 export default Experience;
