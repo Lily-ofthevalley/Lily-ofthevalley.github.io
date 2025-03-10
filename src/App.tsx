1b9bb78e26feed750aa97c8b113172eb707b5fd1
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-10 11:53:23 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import AboutMe from "./layouts/AboutMe";
 import Experience from "./layouts/Experience";
 import Projects from "./layouts/Projects";
 import Technologies from "./layouts/Technologies";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       <Header />
       <Socials />
-      <div className="h-screen w-full flex justify-center mt-36">
+      <div className="w-full flex justify-center mt-36 mb-24">
         <AboutMe />
       </div>
       <div className="w-full flex flex-row justify-center pt-3 pb-3 pl-10 pr-10">
@@ -20,6 +21,7 @@ function App() {
       <div className="w-full flex justify-center pt-3 pb-3">
         <Projects />
       </div>
+      <Footer />
     </div>
   );
 }
