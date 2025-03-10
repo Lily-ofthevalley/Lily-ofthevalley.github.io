3ae2a7355ee0d07a1100de5f1c8320c2569525e2
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-10 11:53:19 +0100

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#795B82] w-full p-4 flex flex-col justify-center items-center">
+      <p>Lilith van der Plas ©2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
