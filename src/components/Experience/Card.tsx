d7624ca2671cf1ca42cd0363c9643cd6518c7815
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-10 12:52:13 +0100

    Update Card.tsx

--- src/components/Experience/Card.tsx
+++ src/components/Experience/Card.tsx
@@ -9,7 +9,11 @@ interface Props {
 const Card = ({ imageUrl, name, timeframe, title, note }: Props) => {
   return (
     <div className="w-full min-h-1/5 h-auto bg-[#AC8DAF] rounded-4xl flex items-center mb-3 p-3">
-      <img src={imageUrl} alt={name} className="w-1/5 h-auto ml-3 mr-3" />
+      <img
+        src={imageUrl}
+        alt={name}
+        className="w-1/5 h-auto ml-3 mr-3 rounded-xl"
+      />
       <div>
         <p className="text-lg font-bold">{name}</p>
         <p>{title}</p>
