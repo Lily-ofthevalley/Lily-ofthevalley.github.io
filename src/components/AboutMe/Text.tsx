ae4eb882c1c21b07619ec82a36cdc8e807433d34
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-03-10 11:41:03 +0100

    Update Text.tsx

--- src/components/AboutMe/Text.tsx
+++ src/components/AboutMe/Text.tsx
@@ -1,7 +1,7 @@
 const Text = () => {
   return (
     <div className="flex flex-col justify-center items-center p-4 w-full h-full">
-      <h1 className="font-extrabold text-5xl mb-3">
+      <h1 className="font-extrabold text-5xl mb-3 text-[#DDB6C6]">
         Hi, I'm Lilith van der Plas
       </h1>
       <h2 className="font-bold text-3xl text-[#AC8DAF]">
